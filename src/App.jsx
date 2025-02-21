b9c673b6508259b3cdb4da06154a86e1ca9fe2ce
unknown <morrix.ken@gmail.com>
2025-02-21 11:51:34 +0800

    feat: Admin

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { ThesisPDFViewer } from './components/pdf';
+
+function App() {
+  return (
+    <div className="App">
+      <ThesisPDFViewer />
+    </div>
+  );
+}
+
+export default App;
