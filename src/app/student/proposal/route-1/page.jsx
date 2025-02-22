77d7f30e381872a4aed4f8efba4a4e690667a493
Bobert <salvo.robertroy@gmail.com>
2025-02-22 22:58:47 +0800

    feat: implemented top menu for ui

--- src/app/student/proposal/route-1/page.jsx
+++ src/app/student/proposal/route-1/page.jsx
@@ -1,7 +1,18 @@
-import React from 'react';
+import TopMenu from '../../../../components/menu/TopMenu';
+import { FileUp } from 'lucide-react';
 
 const Page = () => {
-	return <div>Route 1</div>;
+	const menuItems = [
+		{ label: "Submit File", icon: <FileUp size={25} /> },
+		{ label: "Panel" },
+		{ label: "Adviser" },
+	  ];
+	
+	return (
+		<div>
+			<TopMenu menuItems={menuItems} />
+		</div>
+	);
 };
 
 export default Page;
