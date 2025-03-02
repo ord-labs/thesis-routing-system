322a98074a94c5660764ab609329e49d015a7f13
Kurt Vincent Timajo <djkurttimajo@gmail.com>
2025-03-02 10:43:00 +0800

    chore: removed isAuth in panel and adviser

--- src/app/panel/proposal/route-2/page.jsx
+++ src/app/panel/proposal/route-2/page.jsx
@@ -1,9 +1,8 @@
-'use client'
+'use client';
 
 import PanelAdFileCard from '../../../../components/card/PanelAdFileCard';
 import { useCallback, useEffect, useState } from 'react';
 import { useThesisStore } from '../../../../stores/useThesisStore';
-import isAuth from '../../../../components/isAuth';
 
 const Page = () => {
 	const theses = useThesisStore((state) => state.theses);
@@ -12,31 +11,36 @@ const Page = () => {
 
 	const getThesisPapers = useCallback(async () => {
 		await getAllThesis();
-    }, [getAllThesis]);
+	}, [getAllThesis]);
 
-    useEffect(() => {
-        getThesisPapers();
-    }, [getThesisPapers]); 
+	useEffect(() => {
+		getThesisPapers();
+	}, [getThesisPapers]);
 
 	return (
-		<div className='  flex flex-col justify-center md:items-start'>
+		<div className="  flex flex-col justify-center md:items-start">
 			{loading ? (
-                <div className="flex justify-center w-full items-center h-40">
-                    <div className="w-10 h-10 border-4 border-gray-300 border-t-blue-500 rounded-full animate-spin"></div>
-                </div>
-            ) : (
-				<div className='w-full flex flex-wrap gap-20 justify-center md:justify-start'>
+				<div className="flex justify-center w-full items-center h-40">
+					<div className="w-10 h-10 border-4 border-gray-300 border-t-blue-500 rounded-full animate-spin"></div>
+				</div>
+			) : (
+				<div className="w-full flex flex-wrap gap-20 justify-center md:justify-start">
 					{theses.length > 0 ? (
 						theses.map((thesis) => (
-							<PanelAdFileCard key={thesis.id} role={'panel'} paperId={thesis.id} pdfUrl={thesis.fileUrl} />
+							<PanelAdFileCard
+								key={thesis.id}
+								role={'panel'}
+								paperId={thesis.id}
+								pdfUrl={thesis.fileUrl}
+							/>
 						))
 					) : (
 						<p className="text-gray-500 text-center">No thesis papers found.</p>
 					)}
 				</div>
-            )}
+			)}
 		</div>
 	);
 };
 
-export default isAuth(Page);
+export default Page;
