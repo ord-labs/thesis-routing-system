f0bd9adb6ce38983d7afcbbdd91a7001a734f0ac
Kurt Vincent Timajo <djkurttimajo@gmail.com>
2025-03-20 14:27:05 +0800

    chore: button component w-full

--- src/components/button/TRSButton.jsx
+++ src/components/button/TRSButton.jsx
@@ -4,7 +4,7 @@ const TRSButton = ({ label, onClick, disabled = false }) => {
 	return (
 		<button
 			disabled={disabled}
-			className={`p-2 rounded-lg my-2 text-white text-sm ${
+			className={`w-full p-2 rounded-lg my-2 text-white text-sm ${
 				disabled ? 'bg-gray-500' : 'bg-smccprimary hover:bg-blue-700'
 			} `}
 			onClick={onClick}
